dfa1/rocksdbffm

feat: scoped zero-copy get(key, Mapper) across every DB type + RocksIterator
Adds ReadWriteDB.get(MemorySegment, Mapper) and friends — a scoped,
zero-copy read built on the rocksdb_pinnable_handle_t C API
(rocksdb_get_pinned_v2/_cf_v2, rocksdb_pinnable_handle_get_value,
rocksdb_pinnable_handle_destroy) as an alternative to get(byte[]) for hot
read paths, extended to every DB wrapper in this codebase:

- ReadWriteDB, ReadOnlyDB, TtlDB, SecondaryDB, BlobDB,
  OptimisticTransactionDB — backed by a plain rocksdb_t*, share the
  rocksdb_pinnable_handle_t mechanism via RocksDB.withPinned/withPinnedCf.
- TransactionDB and Transaction — no _v2 equivalent in c.h, so these go
  through the older rocksdb_pinnableslice_t API instead, sharing the same
  lifetime contract via RocksDB.withPinnedCore (parameterized on the
  value/destroy MethodHandle pair for whichever native handle kind
  applies).
- RocksIterator.key(Mapper)/value(Mapper) — same zero-copy-view-scoped-to-
  the-callback idea, turning "escaped view silently reports the next
  position's bytes" into a loud IllegalStateException/WrongThreadException
  instead.

Mapper<R> is the callback: R map(MemorySegment value), invoked with a
read-only view bound to an arena that closes the moment the callback
returns. A null return from the mapper is rejected with NullPointerException
everywhere it's used, rather than silently conflated with "key not found"
(Optional.empty()) or accepted as a valid result.

Also:
- NativeLibrary.lookup grows a Linker.Option... parameter so
  rocksdb_pinnable_handle_get_value/destroy (pure pointer arithmetic, no
  heap-array access) can be marked Linker.Option.critical(false).
  get_pinned_*_v2 stays non-critical since it can block on disk I/O.
- JMH: new Instant-deserialize comparison (byte[] get + manual deserialize
  vs. zero-copy Mapper) and a blob-size sweep (8B..1MB) across byte[] get,
  get(MemorySegment,MemorySegment), and get(key, Mapper) to find the
  crossover point.

Test coverage: Pi

core/src/test/java/io/github/dfa1/rocksdbffm/ReadOnlyDBTest.java
@@ -7,6 +7,8 @@ import java.lang.foreign.Arena;
 import java.lang.foreign.ValueLayout;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -159,6 +161,76 @@ class ReadOnlyDBTest {
 			assertThat(len).isEqualTo(5);
 			assertThat(value.asSlice(0, 5).toArray(ValueLayout.JAVA_BYTE))
 					.isEqualTo("value".getBytes());
+		}
+	}
+
+	// -----------------------------------------------------------------------
+	// get — scoped zero-copy (Mapper)
+	// -----------------------------------------------------------------------
+
+	@Test
+	void get_zeroCopy_returnsValue(@TempDir Path dir) {
+		// Given
+		try (var rw = RocksDB.open(dir)) {
+			rw.put("key".getBytes(), "value".getBytes());
+		}
+
+		try (var ro = RocksDB.openReadOnly(dir);
+		     Arena arena = Arena.ofConfined()) {
+			var key = arena.allocateFrom("key").asSlice(0, 3);
+
+			// When
+			var result = ro.get(key, value -> value.toArray(ValueLayout.JAVA_BYTE));
+
+			// Then
+			assertThat(result).isPresent();
+			assertThat(result.get()).isEqualTo("value".getBytes());
+		}
+	}
+
+	@Test
+	void get_zeroCopy_returnsEmpty_whenKeyAbsent(@TempDir Path dir) {
+		// Given
+		try (var rw = RocksDB.open(dir)) {
+			rw.put("seed".getBytes(), "val".getBytes());
+		}
+
+		try (var ro = RocksDB.openReadOnly(dir);
+		     Arena arena = Arena.ofConfined()) {
+			var key = arena.allocateFrom("missing").asSlice(0, 7);
+
+			// When
+			var result = ro.get(key, value -> value.toArray(ValueLayout.JAVA_BYTE));
+
+			// Then
+			assertThat(result).isEmpty();
+		}
+	}
+
+	@Test
+	void get_zeroCopy_fromColumnFamily(@TempDir Path dir) {
+		// Given
+		try (var rw = RocksDB.open(dir);
+		     var cf = rw.createColumnFamily(ColumnFamilyDescriptor.of("cf1"))) {
+			rw.put(cf, "key".getBytes(), "value".getBytes());
+		}
+
+		List<ColumnFamilyHandle> handles = new ArrayList<>();
+		try (var opts = Options.newOptions();
+		     var ro = RocksDB.openReadOnlyWithColumnFamilies(opts, dir,
+				     List.of(ColumnFamilyDescriptor.of("default"), ColumnFamilyDescriptor.of("cf1")), handles);
+		     Arena arena = Arena.ofConfined()) {
+			var cf1 = handles.get(1);
+			var key = arena.allocateFrom("key").asSlice(0, 3);
+
+			// When
+			var result = ro.get(cf1, key, value -> value.toArray(ValueLayout.JAVA_BYTE));
+
+			// Then
+			assertThat(result).isPresent();
+			assertThat(result.get()).isEqualTo("value".getBytes());
+
+			handles.forEach(ColumnFamilyHandle::close);
 		}
 	}
 

dfa1/rocksdb-ffm

core/src/test/java/io/github/dfa1/rocksdbffm/ReadOnlyDBTest.java
@@ -165,6 +165,76 @@ class ReadOnlyDBTest {
 	}
 
 	// -----------------------------------------------------------------------
+	// get — scoped zero-copy (Mapper)
+	// -----------------------------------------------------------------------
+
+	@Test
+	void get_zeroCopy_returnsValue(@TempDir Path dir) {
+		// Given
+		try (var rw = RocksDB.openReadWrite(dir)) {
+			rw.put("key".getBytes(), "value".getBytes());
+		}
+
+		try (var ro = RocksDB.openReadOnly(dir);
+		     Arena arena = Arena.ofConfined()) {
+			var key = arena.allocateFrom("key").asSlice(0, 3);
+
+			// When
+			var result = ro.get(key, value -> value.toArray(ValueLayout.JAVA_BYTE));
+
+			// Then
+			assertThat(result).isPresent();
+			assertThat(result.get()).isEqualTo("value".getBytes());
+		}
+	}
+
+	@Test
+	void get_zeroCopy_returnsEmpty_whenKeyAbsent(@TempDir Path dir) {
+		// Given
+		try (var rw = RocksDB.openReadWrite(dir)) {
+			rw.put("seed".getBytes(), "val".getBytes());
+		}
+
+		try (var ro = RocksDB.openReadOnly(dir);
+		     Arena arena = Arena.ofConfined()) {
+			var key = arena.allocateFrom("missing").asSlice(0, 7);
+
+			// When
+			var result = ro.get(key, value -> value.toArray(ValueLayout.JAVA_BYTE));
+
+			// Then
+			assertThat(result).isEmpty();
+		}
+	}
+
+	@Test
+	void get_zeroCopy_fromColumnFamily(@TempDir Path dir) {
+		// Given
+		try (var rw = RocksDB.openReadWrite(dir);
+		     var cf = rw.createColumnFamily(ColumnFamilyDescriptor.of("cf1"))) {
+			rw.put(cf, "key".getBytes(), "value".getBytes());
+		}
+
+		List<ColumnFamilyHandle> handles = new ArrayList<>();
+		try (var opts = Options.newOptions();
+		     var ro = RocksDB.openReadOnly(opts, dir,
+				     List.of(ColumnFamilyDescriptor.of("default"), ColumnFamilyDescriptor.of("cf1")), handles);
+		     Arena arena = Arena.ofConfined()) {
+			var cf1 = handles.get(1);
+			var key = arena.allocateFrom("key").asSlice(0, 3);
+
+			// When
+			var result = ro.get(cf1, key, value -> value.toArray(ValueLayout.JAVA_BYTE));
+
+			// Then
+			assertThat(result).isPresent();
+			assertThat(result.get()).isEqualTo("value".getBytes());
+
+			handles.forEach(ColumnFamilyHandle::close);
+		}
+	}
+
+	// -----------------------------------------------------------------------
 	// get — column family overloads
 	// -----------------------------------------------------------------------
 